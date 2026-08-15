8b7b178cbb4b106ac1abf820f2bbd7c7e70c70ea
Tamer-Devoloper <104938736+Tamer-Devoloper@users.noreply.github.com>
2026-08-15 01:29:52 +0100

    Stock

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -1397,9 +1397,7 @@ static int init_domain(struct exynos_cpufreq_domain *domain,
 		domain->max_freq = min(domain->max_freq, val);
 	if (!of_property_read_u32(dn, "min-freq", &val))
 		domain->min_freq = max(domain->min_freq, val);
-
-	if (domain->max_freq == 2002000) domain->max_freq = 2210000;
-
+	
 	/* Get freq-table from device tree and cut the out of range */
 	raw_table_size = of_property_count_u32_elems(dn, "freq-table");
 	if (of_property_read_u32_array(dn, "freq-table",
